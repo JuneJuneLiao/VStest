69fce9613b67c0e66e7da447895ce9399af7d68b
JuneJuneLiao <rita5112632@gmail.com>
2022-10-14 18:08:50 +0800

    1014 commit

--- WindowsFormsApp1011/Form1.Designer.cs
+++ WindowsFormsApp1011/Form1.Designer.cs
@@ -28,21 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_rf = new System.Windows.Forms.Button();
+            this.btn_cf = new System.Windows.Forms.Button();
+            this.lab_type = new System.Windows.Forms.Label();
+            this.dateTimePicker_Star = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker_Stop = new System.Windows.Forms.DateTimePicker();
+            this.cb_df = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btn_rf
+            // 
+            this.btn_rf.Location = new System.Drawing.Point(446, 27);
+            this.btn_rf.Name = "btn_rf";
+            this.btn_rf.Size = new System.Drawing.Size(75, 23);
+            this.btn_rf.TabIndex = 0;
+            this.btn_rf.Text = "Read File";
+            this.btn_rf.UseVisualStyleBackColor = true;
+            this.btn_rf.Click += new System.EventHandler(this.btn_rf_Click);
+            // 
+            // btn_cf
+            // 
+            this.btn_cf.Location = new System.Drawing.Point(539, 27);
+            this.btn_cf.Name = "btn_cf";
+            this.btn_cf.Size = new System.Drawing.Size(75, 23);
+            this.btn_cf.TabIndex = 1;
+            this.btn_cf.Text = "Confirm";
+            this.btn_cf.UseVisualStyleBackColor = true;
+            this.btn_cf.Click += new System.EventHandler(this.btn_cf_Click);
+            // 
+            // lab_type
+            // 
+            this.lab_type.AutoSize = true;
+            this.lab_type.Location = new System.Drawing.Point(22, 31);
+            this.lab_type.Name = "lab_type";
+            this.lab_type.Size = new System.Drawing.Size(36, 15);
+            this.lab_type.TabIndex = 2;
+            this.lab_type.Text = "Type";
+            // 
+            // dateTimePicker_Star
+            // 
+            this.dateTimePicker_Star.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker_Star.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker_Star.Location = new System.Drawing.Point(137, 69);
+            this.dateTimePicker_Star.Name = "dateTimePicker_Star";
+            this.dateTimePicker_Star.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker_Star.TabIndex = 3;
+            // 
+            // dateTimePicker_Stop
+            // 
+            this.dateTimePicker_Stop.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker_Stop.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker_Stop.Location = new System.Drawing.Point(400, 69);
+            this.dateTimePicker_Stop.Name = "dateTimePicker_Stop";
+            this.dateTimePicker_Stop.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker_Stop.TabIndex = 4;
+            // 
+            // cb_df
+            // 
+            this.cb_df.AutoSize = true;
+            this.cb_df.Location = new System.Drawing.Point(25, 75);
+            this.cb_df.Name = "cb_df";
+            this.cb_df.Size = new System.Drawing.Size(90, 19);
+            this.cb_df.TabIndex = 5;
+            this.cb_df.Text = "Date Filter";
+            this.cb_df.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(848, 314);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // cmbType
+            // 
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Items.AddRange(new object[] {
+            "Level",
+            "Category",
+            "Time",
+            "Tags",
+            "Message"});
+            this.cmbType.Location = new System.Drawing.Point(64, 28);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(120, 23);
+            this.cmbType.TabIndex = 7;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(223, 25);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(196, 25);
+            this.txtSearch.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(872, 458);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cb_df);
+            this.Controls.Add(this.dateTimePicker_Stop);
+            this.Controls.Add(this.dateTimePicker_Star);
+            this.Controls.Add(this.lab_type);
+            this.Controls.Add(this.btn_cf);
+            this.Controls.Add(this.btn_rf);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_rf;
+        private System.Windows.Forms.Button btn_cf;
+        private System.Windows.Forms.Label lab_type;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_Star;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_Stop;
+        private System.Windows.Forms.CheckBox cb_df;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.TextBox txtSearch;
     }
 }
 
--- WindowsFormsApp1011/Form1.cs
+++ WindowsFormsApp1011/Form1.cs
@@ -13,22 +13,22 @@ namespace WindowsFormsApp1011
 {
     public partial class Form1 : Form
     {
-        public Button button1;
-        public TextBox textbox1;
-        public ListBox listbox1;
+        //public Button button1;
+        //public TextBox textbox1;
+        //public ListBox listbox1;
 
 
         public Form1()
         {
             InitializeComponent();
 
-            this.button1 = new Button();
-            this.button1.Location = new Point(550, 10);
-            this.button1.Text = "Click Me !";
-            this.button1.Click += new EventHandler(this.button_Click);
+            //this.button1 = new Button();
+            //this.button1.Location = new Point(550, 10);
+            //this.button1.Text = "Click Me !";
+            //this.button1.Click += new EventHandler(this.button_Click);
 
      
-            this.Controls.Add(button1);
+            //this.Controls.Add(button1);
             
             //this.textbox1 = new TextBox();
             //this.listbox1 = new ListBox();
@@ -36,110 +36,76 @@ namespace WindowsFormsApp1011
             //this.Controls.Add(listbox1);
 
         }
-        [STAThread]
-        
-        
+        //[STAThread]
+
+        DataTable dt = new DataTable();
+        //DateTimePicker dtp = new DateTimePicker();
         private void Form1_Load(object sender, EventArgs e)
         {
-
-        }
-        
-
-        private void button_Click (object sender, EventArgs e)
-        {
-            //Console.WriteLine ("hello");
-            //button1.Visible = false;
-
-            DataGridView dataGridView = new DataGridView();
-            
-            // 填滿視窗
-            dataGridView.Dock = DockStyle.Fill;
-            //dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-
-            //自動調欄寬
-            dataGridView.AutoResizeColumns();
-
-            //自動調欄高
-            dataGridView.AutoResizeRows();
-
-
-            DataTable dt = new DataTable();
+            cmbType.SelectedIndex = 0;
+            //dtp.ShowCheckBox = true;
             dt.Columns.Add("Level", typeof(string));
             dt.Columns.Add("Category", typeof(string));
             dt.Columns.Add("Time", typeof(string));
             dt.Columns.Add("Tags", typeof(string));
             dt.Columns.Add("Message", typeof(string));
-            dataGridView.DataSource = dt;
-
-            //width
-            //dataGridView.Columns["Level"].Width = 200;
-            //dataGridView.Columns["Level"].AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader;
-
-
-            using (StreamReader sr = new StreamReader("C:/Users/rita5/source/repos/WindowsFormsApp1011/cx_vis.log"))
-            {
-                string line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine(line.Length);
-
-                    int i = line.IndexOf("@");
-                    int j = line.IndexOf("#");
-                    line = (line.Substring(i + 1)).Substring(0, j - i - 1); //找出@#之間的字串
-                    Console.WriteLine(line.Length);
-
-                    //string[] elements = line.Replace('@', ',').Split(',');
-                    string[] elements = line.Replace(';', ',').Split(new char[] { ',' });         
-                    DataRow dr = dt.NewRow();
-
-
-                    StreamWriter str = new StreamWriter("C:/Users/rita5/source/repos/WindowsFormsApp1011/cx_vis_1.log");
-
-                    str.WriteLine(line);
-                    /*
-                    foreach (string info in elements)
-                    {
-                        str.WriteLine("   {0}", info.Substring(info.IndexOf("@ ") + 1));
-                    }
-                    */
-                    str.Close();
-
-                    
-                    if (dataGridView.Rows.Count == 0)
-                    {
-                        
-                    }
-                    
-                   
-                    if (elements[3] == "@" )
-                    {
-                        dr[3] = elements[3];
-                    }
-                    else
-                    {
-                        dr[3] = null;
-                    }
-
-                    
-
-                    dr[0] = elements[0];
-                    dr[1] = elements[1];
-                    dr[2] = elements[2];
-
-                    dr[3] = elements[3];
-                    //dr[4] = elements[4];
-                    
-
-                    //將資料加入到datatable中
-                    dt.Rows.Add(dr);
-                }
-            }
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[0].MinimumWidth = 90;
+            dataGridView1.Columns[1].MinimumWidth = 90;
+            dataGridView1.Columns[2].MinimumWidth = 120;
+            dataGridView1.Columns[3].MinimumWidth = 90;
+            dataGridView1.Columns[4].MinimumWidth = 180;
+            //dataGridView1.Rows[0].MinimumHeight = 50;
             
-            // 新增到當前的 Form 中
-            this.Controls.Add(dataGridView);
+
         }
 
+        private void btn_rf_Click(object sender, EventArgs e)
+        {
+            string [] lines = File.ReadAllLines(@"C:\Users\rita5\source\repos\WindowsFormsApp1011\cx_vis.log");
+            string [] values;
 
+            for (int i = 0; i < lines.Length; i++)
+            {
+                values = lines[i].ToString().Replace('@',' ').Split(',');
+                
+                string[] row = new string[values.Length];
+
+                for (int j = 0; j< lines.Length; j++)
+                {
+                    row[j] = values[j].Trim();
+                }
+                dt.Rows.Add(row);
+            }
+        }
+
+         
+
+
+        private void btn_cf_Click(object sender, EventArgs e) //confirm 設定
+        {
+            if (string.IsNullOrEmpty(txtSearch.Text))  //confirm 設定 txt
+            {
+                //customersBindingSource.Filter = string.Empty;
+            }
+            else
+            {
+                //customersBindingSource.Filter = string.Format("{0} ='{1}'", cmbType.Text, txtSearch.Text);
+            }
+
+
+
+            /*if (cb_df) //設定時間
+            {
+                DataTime Date1 = dateTimePicker_Star.Value.Date;
+                DataTime Date2 = dateTimePicker_Stop.Value.Date;
+                dataGridView1.RowFilter = String.Format("Date1 > #{0:yyyy-MM-dd HH:mm:ss}# AND Date1 < #{0:yyyy-MM-dd HH:mm:ss}#", Date1,Date2);
+                dataGridView1.DataSource = dt;
+
+                
+
+            }*/
+        }
     }
-   
-   }
+
+}
