f27bf1f7e376d6ef51620e73c5d96e4d797d6912
JuneJuneLiao <rita5112632@gmail.com>
2022-10-28 18:10:07 +0800

    update thread problem

--- WindowsFormsApp1011/Form1.cs
+++ WindowsFormsApp1011/Form1.cs
@@ -53,10 +53,13 @@ namespace WindowsFormsApp1011
             dataGridView1.AllowUserToAddRows = false;
             saveFileButton.Anchor = AnchorStyles.Top |
                                     AnchorStyles.Right;
+            uiEnable(false);
+            readFileButton.Enabled = true;
         }
 
         private void uiEnable(bool enable)
         {
+            readFileButton.Enabled = enable;
             typeComboBox.Enabled = enable;
             searchTextBox.Enabled = enable;
             confirmButton.Enabled = enable;
@@ -66,10 +69,11 @@ namespace WindowsFormsApp1011
             endDateTimePicker.Enabled = enable;
         }
 
+        private OpenFileDialog dialog = new OpenFileDialog();
         // read file
         private void readFileButton_Click(object sender, EventArgs e)       
         {
-            OpenFileDialog dialog = new OpenFileDialog();
+            uiEnable(false);
             dialog.Multiselect = true;
             dialog.Title = "請選擇資料夾";
             // 設定起始目錄
@@ -77,70 +81,58 @@ namespace WindowsFormsApp1011
             // 設定起始目錄為程式目錄
             //dialog.InitialDirectory = Application.StartupPath;  
             dialog.Filter = "所有檔案(*.*)|*.*";
-            Thread thread = new Thread(getDataTableCollection);
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-            readFileButton.Enabled = false;
-            uiEnable(false);
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Thread thread = new Thread(getDataTableCollection);
+                thread.Start();
+            }
+            else
+            {
+                readFileButton.Enabled = true;
+                dialog.Dispose();
+                return;
+            }
         }
 
         private void getDataTableCollection()
         {
             rowItems = new List<RowItem>();
-            OpenFileDialog dialog = new OpenFileDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
+            string[] lines = File.ReadAllLines(dialog.FileName);
+            foreach (string line in lines)
             {
-                string[] lines = File.ReadAllLines(dialog.FileName);
-
-                foreach (string line in lines)
+                RowItem rowItem = new RowItem();
+                rowItem.RowData = line;
+                bool getTags = line.Contains('@');
+                if (getTags == true)
                 {
-                    RowItem rowItem = new RowItem();
-                    rowItem.RowData = line;
-                    bool getTags = line.Contains('@');
-                    if (getTags == true)
-                    {
-                        string[] getData = line.Split(new string[] { "@" }, StringSplitOptions.RemoveEmptyEntries);
-                        string[] getResult = getData[0].Split(',');
-                        rowItem.Level = getResult[0];
-                        rowItem.Category = getResult[1];
-                        rowItem.Time = DateTime.Parse(getResult[2]);
+                    string[] getData = line.Split(new string[] { "@" }, StringSplitOptions.RemoveEmptyEntries);
+                    string[] getResult = getData[0].Split(',');
+                    rowItem.Level = getResult[0];
+                    rowItem.Category = getResult[1];
+                    rowItem.Time = DateTime.Parse(getResult[2]);
 
-                        string[] getResultTags = getData[1].Split(new string[] { "#json " }, StringSplitOptions.RemoveEmptyEntries);
-                        rowItem.Tags = getResultTags[0].Replace(";", ",");
-                        rowItem.Message = getResultTags[1];
-                    }
-                    else
-                    {
-                        string[] getDataElse = line.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-                        rowItem.Level = getDataElse[0];
-                        rowItem.Category = getDataElse[1];
-                        rowItem.Time = DateTime.Parse(getDataElse[2]);
-                        rowItem.Message = getDataElse[3];
-                    }
-                    rowItems.Add(rowItem);
+                    string[] getResultTags = getData[1].Split(new string[] { "#json " }, StringSplitOptions.RemoveEmptyEntries);
+                    rowItem.Tags = getResultTags[0].Replace(";", ",");
+                    rowItem.Message = getResultTags[1];
                 }
-            }
-            else    //if (dialog.ShowDialog() != DialogResult.OK)
-            {
-                Action readEnable = () =>
+                else
                 {
-                    readFileButton.Enabled = true;
-                };
-                Invoke(readEnable);
-                dialog.Dispose(); 
-                return;
+                    string[] getDataElse = line.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                    rowItem.Level = getDataElse[0];
+                    rowItem.Category = getDataElse[1];
+                    rowItem.Time = DateTime.Parse(getDataElse[2]);
+                    rowItem.Message = getDataElse[3];
+                }
+                rowItems.Add(rowItem);
             }
+
+            DataGridViewRow[] rows = rowItems.ConvertAll(x => x.ToRow()).ToArray();
 
             Action action = () =>
             {
-                List<DataGridViewRow> rows = new List<DataGridViewRow>();
-
-                foreach (var rowItem in rowItems)
-                    rows.Add(rowItem.ToRow());
-                dataGridView1.Rows.Clear();
-                dataGridView1.Rows.AddRange(rows.ToArray());
+                dataGridView1.Rows.Clear();                
+                dataGridView1.Rows.AddRange(rows);   
                 uiEnable(true);
-                readFileButton.Enabled = true;
             };
             Invoke(action);
         }
@@ -148,108 +140,166 @@ namespace WindowsFormsApp1011
         // confirm 設定
         private void confirmButton_Click(object sender, EventArgs e)  
         {
-            List<DataGridViewRow> rows = new List<DataGridViewRow>();
-            List<RowItem> filterItems = new List<RowItem>();
-
-            // confirm 設定 txt 篩選
-            if (typeComboBox.Text == "Level")
-            {
-                filterItems = rowItems.FindAll(x => x.Level.ToLower() == searchTextBox.Text);
-                rows = filterItems.ConvertAll(x => x.ToRow());
-
-                if (string.IsNullOrEmpty(searchTextBox.Text))
-                {
-                    MessageBox.Show("請輸入文字 !", "顯示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    rows = rowItems.ConvertAll(x => x.ToRow());
-                }
-            }
-            if (typeComboBox.Text == "Category" )
-            {
-                filterItems = rowItems.FindAll(x => x.Category.ToLower() == searchTextBox.Text);
-                rows = filterItems.ConvertAll(x => x.ToRow());
-
-                if (string.IsNullOrEmpty(searchTextBox.Text))
-                {
-                    MessageBox.Show("請輸入文字 !", "顯示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    rows = rowItems.ConvertAll(x => x.ToRow());
-                }
-            }
-            if (typeComboBox.Text == "Tags")
-            {
-                filterItems = rowItems.FindAll(x => x.Tags == searchTextBox.Text);
-                rows = filterItems.ConvertAll(x => x.ToRow());
-
-                if (string.IsNullOrEmpty(searchTextBox.Text))
-                {
-                    MessageBox.Show("請輸入文字 !", "顯示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    rows = rowItems.ConvertAll(x => x.ToRow());
-                }
-            }
-            if (typeComboBox.Text == "Message")
-            {
-                filterItems = rowItems.FindAll(x => x.Message.ToLower().Contains(searchTextBox.Text));
-                //for (int i = 0; i < dataGridView1.Rows.Count; i++)
-                //    searchTextBox.Text = dataGridView1.Rows[i].Cells[4].Value.ToString();
-                rows = filterItems.ConvertAll(x => x.ToRow());
-
-                if (string.IsNullOrEmpty(searchTextBox.Text))
-                {
-                    MessageBox.Show("請輸入文字 !", "顯示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    rows = rowItems.ConvertAll(x => x.ToRow());
-                }
-            }
-            dataGridView1.Rows.Clear();
-            dataGridView1.Rows.AddRange(rows.ToArray());
-        }
-
-        private void dateFilterCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-            Thread thread = new Thread(dateFilterCheck);
+            uiEnable(false);
+            Thread thread = new Thread(confirmThread);
             thread.SetApartmentState(ApartmentState.STA);
             thread.Start();
         }
 
-        private void dateFilterCheck()
+        private List<DataGridViewRow> rows = new List<DataGridViewRow>();
+        private List<RowItem> filterItems = new List<RowItem>();
+        private void confirmThread ()
         {
-            List<DataGridViewRow> rows = new List<DataGridViewRow>();
-            List<RowItem> filterItems = new List<RowItem>();
+            Action confirm = () =>
+            {
+                // confirm 設定 txt 篩選
+                if (typeComboBox.Text == "Level")
+                {
+                    filterItems = rowItems.FindAll(x => x.Level.ToLower() == searchTextBox.Text);
+                    rows = filterItems.ConvertAll(x => x.ToRow());
 
+                    if (string.IsNullOrEmpty(searchTextBox.Text))
+                    {
+                        MessageBox.Show("請輸入文字 !", "顯示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        rows = rowItems.ConvertAll(x => x.ToRow());
+                    }
+                }
+                else if (typeComboBox.Text == "Category")
+                {
+                    filterItems = rowItems.FindAll(x => x.Category.ToLower() == searchTextBox.Text);
+                    rows = filterItems.ConvertAll(x => x.ToRow());
+
+                    if (string.IsNullOrEmpty(searchTextBox.Text))
+                    {
+                        MessageBox.Show("請輸入文字 !", "顯示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        rows = rowItems.ConvertAll(x => x.ToRow());
+                    }
+                }
+                else if (typeComboBox.Text == "Tags")
+                {
+                    filterItems = rowItems.FindAll(x => x.Tags == searchTextBox.Text);
+                    rows = filterItems.ConvertAll(x => x.ToRow());
+
+                    if (string.IsNullOrEmpty(searchTextBox.Text))
+                    {
+                        MessageBox.Show("請輸入文字 !", "顯示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        rows = rowItems.ConvertAll(x => x.ToRow());
+                    }
+                }
+                else if (typeComboBox.Text == "Message")
+                {
+                    filterItems = rowItems.FindAll(x => x.Message.ToLower().Contains(searchTextBox.Text));
+                    rows = filterItems.ConvertAll(x => x.ToRow());
+
+                    if (string.IsNullOrEmpty(searchTextBox.Text))
+                    {
+                        MessageBox.Show("請輸入文字 !", "顯示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        rows = rowItems.ConvertAll(x => x.ToRow());
+                    }
+                }
+                dataGridView1.Rows.Clear();
+                dataGridView1.Rows.AddRange(rows.ToArray());
+                uiEnable(true);
+            };
+            Invoke(confirm);           
+        }
+
+        private void dateFilterCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            uiEnable(false);
             if (dateFilterCheckBox.Checked == true)
             {
-                var dateTimeStart = startDateTimePicker.Value;
-                var dateTimeEnd = endDateTimePicker.Value;
-                var filterTimeItems = filterItems.FindAll(x => x.Time >= dateTimeStart && x.Time <= dateTimeEnd);
-                rows = filterTimeItems.ConvertAll(x => x.ToRow());
+                Thread thread = new Thread(dateFilterCheck);
+                thread.Start();
             }
             else
             {
-                rows = rowItems.ConvertAll(x => x.ToRow());
+                Thread thread = new Thread(dateFilterUnchecked);
+                thread.Start();
             }
-            Action check = () =>
-            {
-                dataGridView1.Rows.Clear();
-                dataGridView1.Rows.AddRange(rows.ToArray());
-            };
-            Invoke(check);
         }
 
-        // write file thread
-        private void saveFileButton_Click(object sender, EventArgs e)       
+        private void dateFilterCheck()
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            var dateTimeStart = startDateTimePicker.Value;
+            var dateTimeEnd = endDateTimePicker.Value;
+            if (string.IsNullOrEmpty(searchTextBox.Text))
+            {
+                var filterTimeItems = rowItems.FindAll(x => x.Time >= dateTimeStart && x.Time <= dateTimeEnd);
+                rows = filterTimeItems.ConvertAll(x => x.ToRow());
+                Action checkNoSearchTextBox = () =>
+                {
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Rows.AddRange(rows.ToArray());
+                    uiEnable(true);
+                };
+                Invoke(checkNoSearchTextBox);
+            }
+            else
+            {
+                var filterTimeItems = filterItems.FindAll(x => x.Time >= dateTimeStart && x.Time <= dateTimeEnd);
+                rows = filterTimeItems.ConvertAll(x => x.ToRow());
+                Action check = () =>
+                {
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Rows.AddRange(rows.ToArray());
+                    uiEnable(true);
+                };
+                Invoke(check);
+            }  
+        }
+        private void dateFilterUnchecked()
+        {
+            if (string.IsNullOrEmpty(searchTextBox.Text))
+            {
+                rows = rowItems.ConvertAll(x => x.ToRow());
+                Action uncheck = () =>
+                {
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Rows.AddRange(rows.ToArray());
+                    uiEnable(true);
+                };
+                Invoke(uncheck);
+            }
+            else
+            {
+                rows = filterItems.ConvertAll(x => x.ToRow());
+                Action uncheck = () =>
+                {
+                    dataGridView1.Rows.Clear();
+                    dataGridView1.Rows.AddRange(rows.ToArray());
+                    uiEnable(true);
+                };
+                Invoke(uncheck);
+            }                
+        }
+
+        private SaveFileDialog saveFileDialog = new SaveFileDialog();
+        // write file thread
+        private void saveFileButton_Click(object sender, EventArgs e)
+        {
+            uiEnable(false);
             saveFileDialog.Filter = "Text file (*.txt)|*.txt|All files (*.*)|*.*";
             saveFileDialog.InitialDirectory = @"C:\Users\rita5\source\repos\WindowsFormsApp1011";
             saveFileDialog.RestoreDirectory = true;
             saveFileDialog.Title = "另存新檔";
-            Thread threadWrite = new Thread(threadWriteFile);
-            threadWrite.SetApartmentState(ApartmentState.STA);
-            threadWrite.Start();
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Thread threadWrite = new Thread(threadWriteFile);
+                threadWrite.SetApartmentState(ApartmentState.STA);
+                threadWrite.Start();
+            }
+            else
+            {
+                uiEnable(true);
+                dialog.Dispose();
+                return;
+            }
         }
-            
+
         private void threadWriteFile()
         {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            Action saveFile = () =>
             {
                 FileStream fs = new FileStream(saveFileDialog.FileName, FileMode.Create);
                 StreamWriter sw = new StreamWriter(fs);
@@ -259,20 +309,19 @@ namespace WindowsFormsApp1011
                     MessageBox.Show("沒有數據，輸出失敗", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
                 else
-                { 
+                {
                     for (int i = 0; i < dataGridView1.Rows.Count; i++)//
                     {
-                            sw.Write(dataGridView1.Rows[i].Cells[5].Value.ToString());
-                            sw.WriteLine("");                       
+                        sw.Write(dataGridView1.Rows[i].Cells[5].Value.ToString());
+                        sw.WriteLine("");
                     }
                     // clean 緩衝區
                     //sw.Flush();
                     sw.Close();
                     fs.Close();
                 }
-            }
+            };
+            Invoke(saveFile);
         }
-
-
     }
 }
